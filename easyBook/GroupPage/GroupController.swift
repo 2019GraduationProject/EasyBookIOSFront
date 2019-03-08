350ea9776652b2a9b95e05822dd52daaf0d2b789
sherley_huang <834998556@qq.com>
2019-03-08 13:41:46 +0800

    群组页面新增搜索群组功能

--- easyBook/GroupPage/GroupController.swift
+++ easyBook/GroupPage/GroupController.swift
@@ -10,18 +10,25 @@ import UIKit
 
 /// ⚠️注：由于使用了 Search Bar and Search Display Controller 组件，
 ///      所有的 tableView 引用都要加上self.tableView，否则程序会崩溃
-class GroupController: UITableViewController {
+class GroupController: UITableViewController, UISearchResultsUpdating {
     
-    @IBOutlet weak var groupSearchBar: UISearchBar!
+    var searchController: UISearchController!
+    
     
     var naviBarShadowImage = UIImage(named: "separator") // 存储导航条图片
-    var initialOffsetY : CGFloat! // 初始位移量
+    var initialOffsetY: CGFloat! // 初始位移量
     var isInitialCompute = true // 是否是第一次计算初始位移的标志
     
-    var actionImgList = ["new_invitation_40x40_","new_group_40x40_"]
-    var actionTitleList = ["新的邀请", "新建群组"]
+    let actionImgList = ["new_invitation_40x40_","new_group_40x40_"]
+    let actionTitleList = ["新的邀请", "新建群组"]
+    let createdGroupNameList = ["机器学习", "软工大作业"]
+    let attendedGroupNameList = ["出国留学申请", "movies", "本科毕设", "设计design"]
     
-
+    // 保存搜索结果
+    var createdSearchResult: [String] = []
+    var attendedSearchResult: [String] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,15 +36,28 @@ class GroupController: UITableViewController {
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
         
+        // 修改每个 section 之间的间距：修改 section 的 footer 的大小
+        tableView.sectionFooterHeight = 0
+        
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        tableView.tableHeaderView = searchController.searchBar
+        // 设置搜索条外观
+        searchController.searchBar.tintColor = UIColor(named: "themeColor")
+        searchController.searchBar.placeholder = "搜索群组"
+        searchController.searchBar.searchBarStyle = .minimal
+        searchController.dimsBackgroundDuringPresentation = false // 搜索背景不变暗
+        
+        
         // 去掉 search bar 上下两边的分割线
-        groupSearchBar.setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+//        groupSearchBar.setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         // 去掉第一个 section 最上方的分割线
-        if let cell = tableView.cellForRow(at: [0, 0]) {
+        if let cell = self.tableView.cellForRow(at: [0, 0]) {
             var seperatorId = 0
             for subView in cell.subviews {
                 if subView.bounds.height < 1 {
@@ -64,7 +84,7 @@ class GroupController: UITableViewController {
             initialOffsetY = scrollView.contentOffset.y
             isInitialCompute = false
         }
-        
+
         if scrollView.contentOffset.y > initialOffsetY {
             navigationController?.navigationBar.shadowImage = naviBarShadowImage
             navigationController?.navigationBar.setBackgroundImage(nil, for: .default)
@@ -73,53 +93,96 @@ class GroupController: UITableViewController {
             navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         }
     }
+    
+    
+    // MARK: - UISearchResultsUpdating
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if var text = searchController.searchBar.text {
+            text = text.trimmingCharacters(in: .whitespaces)
+            searchFilter(text: text)
+            tableView.reloadData()
+        }
+    }
+    
+    func searchFilter(text: String) {
+        createdSearchResult = createdGroupNameList.filter({ (groupName) -> Bool in
+            return groupName.localizedCaseInsensitiveContains(text)
+        })
+        
+        attendedSearchResult = attendedGroupNameList.filter({ (groupName) -> Bool in
+            return groupName.localizedStandardContains(text)
+        })
+    }
 
     
     // MARK: - Table View Data Source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 3
+        return searchController.isActive ? 2 : 3
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        switch section {
-        case 0:
-            return 2
-        case 1:
-            return 3
-        default:
-            return 5
+        if searchController.isActive {
+            if section == 0 {
+                return createdSearchResult.count
+            } else {
+                return attendedSearchResult.count
+            }
+        } else {
+            switch section {
+            case 0:
+                return 2
+            case 1:
+                return createdGroupNameList.count
+            default:
+                return attendedGroupNameList.count
+            }
         }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch indexPath.section {
-        case 0:
-            let reuseIdentifier = String(describing: GroupBasicCell.self)
-            let cell = self.tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! GroupBasicCell
-
-            cell.titleImageView.image = UIImage(named: actionImgList[indexPath.row])
-            cell.titleLabel.text = actionTitleList[indexPath.row]
-
-            return cell
-        case 1:
-            let reuseIdentifier = String(describing: GroupBasicCell.self)
-            let cell = self.tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! GroupBasicCell
-            
-            cell.titleImageView.image = UIImage(named: "default_group_avatar_40x40_")
-            cell.titleLabel.text = "群组1"
-            
-            return cell
-        default:
-            let reuseIdentifier = String(describing: GroupBasicCell.self)
-            let cell = self.tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! GroupBasicCell
-            
-            cell.titleImageView.image = UIImage(named: "default_group_avatar_40x40_")
-            cell.titleLabel.text = "群组2"
-            
-            return cell
+        if searchController.isActive {
+            if indexPath.section == 0 {
+                let reuseIdentifier = String(describing: MyGroupCell.self)
+                let cell = self.tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! MyGroupCell
+                
+                cell.groupNameLabel.text = createdSearchResult[indexPath.row]
+                
+                return cell
+            } else {
+                let reuseIdentifier = String(describing: MyGroupCell.self)
+                let cell = self.tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! MyGroupCell
+                
+                cell.groupNameLabel.text = attendedSearchResult[indexPath.row]
+                
+                return cell
+            }
+        } else {
+            switch indexPath.section {
+            case 0:
+                let reuseIdentifier = String(describing: GroupBasicCell.self)
+                let cell = self.tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! GroupBasicCell
+                
+                cell.titleImageView.image = UIImage(named: actionImgList[indexPath.row])
+                cell.titleLabel.text = actionTitleList[indexPath.row]
+                
+                return cell
+            case 1:
+                let reuseIdentifier = String(describing: MyGroupCell.self)
+                let cell = self.tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! MyGroupCell
+                
+                cell.groupNameLabel.text = createdGroupNameList[indexPath.row]
+                
+                return cell
+            default:
+                let reuseIdentifier = String(describing: MyGroupCell.self)
+                let cell = self.tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! MyGroupCell
+                
+                cell.groupNameLabel.text = attendedGroupNameList[indexPath.row]
+                
+                return cell
+            }
         }
     }
     
@@ -128,67 +191,63 @@ class GroupController: UITableViewController {
         self.tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if searchController.isActive {
+            if section == 0 && createdSearchResult.count > 0 {
+                return "我创建的群组"
+            } else if (section == 1 && attendedSearchResult.count > 0) {
+                return "我加入的群组"
+            } else {
+                return nil
+            }
+        } else {
+            switch section {
+            case 1:
+                return "我创建的群组"
+            case 2:
+                return "我加入的群组"
+            default:
+                return nil
+            }
+        }
+    }
+    
+    /// 修改每个 section 的 header 的高度
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return 0
+        } else {
+            return 30
+        }
+    }
+    
     /// 创建每个section的顶端label
     ///
     /// - Parameters:
     ///   - tableView: UITableView
     ///   - section: section的ID
     /// - Returns: headerView
-    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let headerView = UIView()
-        headerView.backgroundColor = UIColor.clear
-        
-        let headerLabel = UILabel(frame: CGRect(x: 20, y: 0, width: 100, height: 22))
-        headerLabel.textColor = UIColor(named: "themeGray")
-        headerLabel.font = UIFont.systemFont(ofSize: 11)
-        headerView.addSubview(headerLabel)
-        
-        switch section {
-        case 1:
-            headerLabel.text = "我创建的群组"
-        case 2:
-            headerLabel.text = "我加入的群组"
-        default:
-            break
-        }
-        
-        return headerView
-    }
+//    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let headerView = UIView()
+//        headerView.backgroundColor = UIColor.clear
+//
+//        let headerLabel = UILabel(frame: CGRect(x: 20, y: 0, width: 100, height: 22))
+//        headerLabel.textColor = UIColor(named: "themeGray")
+//        headerLabel.font = UIFont.systemFont(ofSize: 11)
+//        headerView.addSubview(headerLabel)
+//
+//        switch section {
+//        case 1:
+//            headerLabel.text = "我创建的群组"
+//        case 2:
+//            headerLabel.text = "我加入的群组"
+//        default:
+//            break
+//        }
+//
+//        return headerView
+//    }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
---
+++ easyBook/GroupPage/MyGroupCell.swift
@@ -0,0 +1,26 @@
+//
+//  MyGroupCell.swift
+//  easyBook
+//
+//  Created by 黄小白 on 2019/3/8.
+//  Copyright © 2019 Sherley Huang's studio. All rights reserved.
+//
+
+import UIKit
+
+class MyGroupCell: UITableViewCell {
+    
+    @IBOutlet weak var groupNameLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
